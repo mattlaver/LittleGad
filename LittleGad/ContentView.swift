e95120cb2afb40d9df9e393e782ba81dc3e1684a
Matt <mattlaver@gmail.com>
2022-11-16 23:20:35 +0000

    Initial Commit

---
+++ LittleGad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LittleGad
+//
+//  Created by Matt Laver on 16/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LittleGad/LittleGadApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleGadApp.swift
+//  LittleGad
+//
+//  Created by Matt Laver on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LittleGadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
